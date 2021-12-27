4e4010d0c6e15f8ccbb3aad7c9d2b0127e3be76d
Oleg White <5998684+whitechno@users.noreply.github.com>
2021-12-26 16:19:12 -0800

    Minor edits in TestMain_00_Random.scala

--- simple-spark-submit/src/test/scala/iskra/TestMain_00_Random.scala
+++ simple-spark-submit/src/test/scala/iskra/TestMain_00_Random.scala
@@ -6,10 +6,12 @@ object TestMain_00_Random extends App {
   val seed  = 1
   val bound = 10
   val r     = new Random(seed)
-  (1 to 10).foreach { i => println(s"$i: ${r.nextInt(bound)}") }
+  (1 to 10).foreach { i => println(f"$i%2d: ${r.nextInt(bound)}") }
 
   val r2 = new Random(seed)
-  (1 to 10).foreach { i => println(s"$i: ${r2.nextInt(bound)}, ${r2.nextGaussian}") }
+  (1 to 10).foreach { i =>
+    println(f"$i%2d: ${r2.nextInt(bound)}, ${r2.nextGaussian}")
+  }
 
   val ct: Long = java.time.Instant.now.getEpochSecond
   println("ct = " + ct)
