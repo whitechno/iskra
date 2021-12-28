43697d002aad17828b87cc60ec52d361c76adc32
Oleg White <5998684+whitechno@users.noreply.github.com>
2021-12-28 14:11:59 -0800

    Added TestMain_01_GraphX_Overview and extended SparkRunner.

--- spark-runner/src/main/scala/iskra/runner/SparkRunner.scala
+++ spark-runner/src/main/scala/iskra/runner/SparkRunner.scala
@@ -1,8 +1,8 @@
 package iskra.runner
 
-import org.apache.spark.sql.{ Dataset, SparkSession }
-import org.apache.spark.sql.functions.expr
+import org.apache.spark.sql.SparkSession
 import org.apache.log4j.{ Level, Logger }
+import org.apache.spark.SparkContext
 
 case class SparkRunner(risc: RunnerInputSparkConfig = RunnerInputSparkConfig()) {
 
@@ -21,26 +21,25 @@ case class SparkRunner(risc: RunnerInputSparkConfig = RunnerInputSparkConfig()) 
     builder = builder.config("spark.driver.allowMultipleContexts", "false")
 
     val sparkSession: SparkSession = builder.getOrCreate()
+    val sparkContext: SparkContext = sparkSession.sparkContext
+    // sparkSession.conf.get(key = "spark.master", default = "-")
+    val sparkMaster: String     = sparkContext.master
+    val defaultParallelism: Int = sparkContext.defaultParallelism
 
     println(
-      "\n\t*** Spark " +
-        util.Properties.versionNumberString + "/" + sparkSession.version +
-        " running on " +
-        sparkSession.conf.get(key = "spark.master", default = "-") +
-        " with " + sparkSession.sparkContext.defaultParallelism + " cores" +
-        " ***\n" +
-        sparkSession.sparkContext.uiWebUrl
-          .map("\tuiWebUrl at " + _ + "\n")
-          .getOrElse("")
+      s"\n\t*** Spark ${sparkSession.version} " +
+        s"(Scala ${util.Properties.versionNumberString})" +
+        s" running on ${sparkMaster} with ${defaultParallelism} cores ***\n" +
+        s"\t    applicationId=${sparkContext.applicationId}\n" +
+        sparkContext.uiWebUrl.map("\t    uiWebUrl at " + _ + "\n").getOrElse("")
     )
 
     sparkSession
   }
 
-  def stopSpark(): Unit =
-    if (risc.sparkStopWhenDone) {
-      println("\n\t*** Stopping Spark Session ***\n")
-      spark.stop()
-    }
+  def stopSpark(): Unit = if (risc.sparkStopWhenDone) {
+    println("\n\t*** Stopping Spark Session ***\n")
+    spark.stop()
+  }
 
 }
---
+++ x-graphx/src/test/scala/iskra/xgraphx/TestMain_01_GraphX_Overview.scala
@@ -0,0 +1,113 @@
+package iskra.xgraphx
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.graphx.{
+  Edge,
+  EdgeContext,
+  EdgeTriplet,
+  Graph,
+  VertexId,
+  VertexRDD
+}
+import iskra.runner.{ RunnerInputSparkConfig, SparkRunner }
+import org.apache.spark.SparkContext
+import org.apache.spark.sql.SparkSession
+
+object TestMain_01_GraphX_Overview {
+  def main(args: Array[String]): Unit = {
+    val sr: SparkRunner = SparkRunner(risc =
+      RunnerInputSparkConfig(master = Some("local[*]"), sparkStopWhenDone = true)
+    )
+
+    run1(sr)
+
+    sr.stopSpark()
+  }
+
+  /*
+  In some cases it may be desirable to have vertices with different property types
+  in the same graph. This can be accomplished through inheritance.
+  For example to model users and products as a bipartite graph we might do the following:
+  class VertexProperty()
+  case class UserProperty(name: String) extends VertexProperty
+  case class ProductProperty(name: String, price: Double) extends VertexProperty
+   // The graph might then have the type:
+    var graph: Graph[VertexProperty, String] = null
+   */
+
+  def run1(sr: SparkRunner): Unit = {
+    import sr.spark.implicits._
+    val sc: SparkContext = sr.spark.sparkContext
+
+    // Create an RDD for the vertices
+    val users: RDD[(VertexId, (String, String))] = sc.parallelize(
+      Seq(
+        (3L, ("rxin", "student")),
+        (7L, ("jgonzal", "postdoc")),
+        (5L, ("franklin", "prof")),
+        (2L, ("istoica", "prof"))
+      )
+    )
+    // Create an RDD for edges
+    val relationships: RDD[Edge[String]] = sc.parallelize(
+      Seq(
+        Edge(3L, 7L, "collab"),
+        Edge(5L, 3L, "advisor"),
+        Edge(2L, 5L, "colleague"),
+        Edge(5L, 7L, "pi")
+      )
+    )
+    // Define a default user in case there are relationship with missing user
+    val defaultUser = ("John Doe", "Missing")
+    // Build the initial Graph
+    val graph: Graph[(String, String), String] =
+      Graph(vertices = users, edges = relationships, defaultVertexAttr = defaultUser)
+
+    ppGraph(graph)
+
+    // Count all users which are postdocs
+    println(
+      "postdoc cnt: " +
+        graph.vertices.filter { case (_, (_, pos)) => pos == "postdoc" }.count()
+    )
+    // Count all the edges where src > dst
+    println(
+      "non-canonical edges cnt: " +
+        graph.edges.filter(e => e.srcId > e.dstId).count()
+    )
+
+    // Use the triplets view to create an RDD of facts.
+    val facts: RDD[String] = graph.triplets.map { triplet =>
+      s"${triplet.srcAttr._1}(${triplet.srcId}) is the ${triplet.attr}" +
+        s" of ${triplet.dstAttr._1}(${triplet.dstId})"
+    }
+    facts.collect().foreach(println)
+
+    // Use the implicit GraphOps.inDegrees operator.
+    // Note: Vertices with no in-edges are not returned in the resulting RDD.
+    val inDegrees: VertexRDD[Int] = graph.inDegrees
+    inDegrees.toDF("vid", "inDegree").show()
+
+//    graph.joinVertices
+
+  }
+
+  def ppGraph(graph: Graph[_, _]): Unit = {
+    val sparkSession: SparkSession = SparkSession.active
+    println(sparkSession.sparkContext.applicationId)
+    import sparkSession.implicits._
+    graph.vertices
+      .map { case (id, attr) => (id, attr.toString) }
+      .toDF("id", "attr")
+      .sort("id")
+      .show()
+    graph.edges
+      .map { case Edge(srcId, dstId, attr) =>
+        Edge[String](srcId, dstId, attr.toString)
+      }
+      .toDF()
+      .sort("srcId", "dstId")
+      .show()
+  }
+
+}
